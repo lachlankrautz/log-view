ede0c8471c77f7e4604bca3f81dc67f08c084713
Lachlan Krautz <lachlan.krautz@gmail.com>
2018-10-28 13:58:50 +1000

    still have weird msys path issues

--- log-view.go
+++ log-view.go
@@ -3,23 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"runtime"
+	"strings"
 )
 
 func main() {
-	args := []string{"rex logs"}
-	if runtime.GOOS == "windows" {
-		args = append([]string{
-			"C:\\msys64\\usr\\bin\\env.exe",
-			"MSYSTEM=MINGW64",
-			"C:\\msys64\\usr\\bin\\bash.exe",
-			"-l",
-			"-c"},
-			args...)
-	}
-
-	fmt.Print(args)
+	args := getArgs()
 
 	cmd := exec.Command(args[0], args[1:]...)
 	out, err := cmd.CombinedOutput()
@@ -27,4 +18,25 @@ func main() {
 	if err != nil {
 		log.Fatal("process failed: ", err)
 	}
+}
+
+func getArgs() []string {
+	// get logs from rex
+	args := []string{"rex", "logs"}
+
+	// append service names to filter logs
+	args = append(args, os.Args[1:]...)
+
+	// if windows run everything in msys shell
+	if runtime.GOOS == "windows" {
+		args = []string{
+			"C:\\msys64\\usr\\bin\\env.exe",
+			"MSYSTEM=MINGW64",
+			"C:\\msys64\\usr\\bin\\bash.exe",
+			"-l",
+			"-c",
+			strings.Join(args, " ")}
+	}
+
+	return args
 }
